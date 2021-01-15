c266e265c162550d3837f3a6f2f6fd47fb916b6e
Carson <gamercarsonzero@gmail.com>
2021-01-15 18:56:44 -0500

    Added command line text and percentage of video scanned.

--- video_to_slide.py
+++ video_to_slide.py
@@ -2,7 +2,7 @@ import cv2
 from PIL import Image
 from PIL import ImageChops
 import math
-import sys
+import sys, argparse
 
 def compare_frames(frame1, frame2):
     """Returns a percentage of how different the frames are."""
@@ -10,7 +10,6 @@ def compare_frames(frame1, frame2):
 
     ndif = sum(diff.getdata(0)) + sum(diff.getdata(1)) + sum(diff.getdata(2))
     ncomponents = frame1.size[0] * frame2.size[1] * len(frame1.getbands())
-    #print(f"Diff between frames: {((ndif / 255.0 * 100) / ncomponents)}")
     return ((ndif / 255.0 * 100) / ncomponents)
 
 
@@ -28,26 +27,24 @@ def read_frame(capture_obj, gray=False):
 
 def main(args):
     """Arg 1 is the video to convert. Arg 2 is playback rate. Arg 3 is threshold needed to the slide add."""
-    if len(args) < 1:
-        return
-    print(args[0])
-    cap = cv2.VideoCapture(args[0])
+    print(args.filename)
+    cap = cv2.VideoCapture(args.filename)
 
     # list of all frames
     slides = []
 
     # % difference needed to trigger a new frame add
-    PLAY_BACK_RATE = float(args[1])
-    TRESH = float(args[2])
+    PLAY_BACK_RATE = float(args.playbackrate)
+    TRESH = float(args.thresh)
 
     cap.read()
     FRAME_RATE = cap.get(cv2.CAP_PROP_FPS)
-    print(f'FRAME_RATE: {FRAME_RATE}')
+    print('FRAME_RATE: {}'.format(FRAME_RATE))
 
     cap.set(cv2.CAP_PROP_POS_AVI_RATIO,1)
     cap.read()
     FRAME_COUNT = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-    print(f'FRAME_COUNT: {FRAME_COUNT}')
+    print('FRAME_COUNT: {}'.format(math.floor(FRAME_COUNT)))
 
     frames = []
     # init with first frame
@@ -61,7 +58,7 @@ def main(args):
 
     cap.set(1,frame_no)
     while (True): 
-        print(f'Frame: {math.floor(frame_no)}')
+        print('Frame: {} ({:3.2f}%)'.format( math.floor(frame_no), math.floor(frame_no)/math.floor(FRAME_COUNT)*100 ) )
         # Capture frame
         
         cap.set(1,math.floor(frame_no))
@@ -74,7 +71,6 @@ def main(args):
             slides.append(frames[-1])
 
         if frame_no >= FRAME_COUNT - 1:
-            print("Exited")
             break
 
         frame_no = frame_no + FRAME_RATE*PLAY_BACK_RATE
@@ -85,9 +81,25 @@ def main(args):
     # release the video capture object 
     cap.release()
     # Closes all the windows currently opened.
-    cv2.destroyAllWindows()    
+    cv2.destroyAllWindows()
 
-    slides[0].save("out.pdf", save_all=True, append_images=slides[1:])
+    print("Saving")
+    try:
+        slides[0].save("out.pdf", save_all=True, append_images=slides[1:])
+    except Exception:
+        print("Failed to save.")
+    
+    print("Saved to out.pdf!")
 
 if __name__ == "__main__":
-    main(sys.argv[1:]) #Pass in the cmd line arguments after filename
+    parser = argparse.ArgumentParser(description="Convert Video to pdf slideshow.",)
+
+    parser.add_argument("filename", metavar="filename", type=str, help="Name of the video file to convert.")
+    parser.add_argument("playbackrate", metavar="--speed", type=float, nargs="?", default=2.0, help="Number of seconds between frames read. Default is 2.")
+    parser.add_argument("thresh", metavar="--threshold", type=float, nargs="?", default=5.0, help="Percent difference needed to detect a frame change. Default of 5 percent.")
+
+    args = parser.parse_args()
+
+    print(args)
+
+    main(args) #Pass in the cmd line arguments after filename
